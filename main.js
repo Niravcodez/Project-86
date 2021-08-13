13da017d54e5b077e7ca775c3101424d0ad1c8e2
Niravcodez <84302842+Niravcodez@users.noreply.github.com>
2021-08-12 17:22:47 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 var canvas = new fabric.Canvas('myCanvas');
- var x= document.getElementById("myAudio");
+var x = document.getElementById("myAudio");
 
 function new_image()
 {
-	fabric.new_image.fromURL ('BirthdayImage.jpg', function(Img) { 
+	fabric.Image.fromURL('BirthdayImage.jpg', function(Img) { 
     block_image_object = Img;
 
     block_image_object.scaleToWidth(700);
     block_image_object.scaleToHeight(510);
-    block_image_object.scaleToHeight({
+    block_image_object.set({
     top:0,
     left:0
     });
